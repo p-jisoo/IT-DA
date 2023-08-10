e64214fcb55974ae23a1d9f43cdedcfd609fdb6f
hhhhhh <flap04@naver.com>
2023-08-10 10:08:45 +0900

    Feat : createBoard,detailBoard, login, deleteMember, deleteMemberPopup UI add

---
+++ donghang/clx-src/detailBoard.js
@@ -0,0 +1,16 @@
+/************************************************
+ * detailBoard.js
+ * Created at 2023. 8. 8. 오전 10:04:40.
+ *
+ * @author USER
+ ************************************************/
+
+/*
+ * "신청하기" 버튼에서 animationend 이벤트 발생 시 호출.
+ * 애니메이션 종료 후 발생하는 이벤트.
+ */
+function onButtonAnimationend(e){
+	var button = e.control;
+	
+	
+}
