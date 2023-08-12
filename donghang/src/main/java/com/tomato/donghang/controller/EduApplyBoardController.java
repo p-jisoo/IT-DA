5f6d9f0976e0b6e77c80ca810014d1287e07e24c
p-jisoo <vkrwltn@nate.com>
2023-08-12 17:36:57 +0900

    Feat : pagination class add
    페이지이동 기능 추가를 위한 클래스 생성과 컨트롤러 수정중

--- donghang/src/main/java/com/tomato/donghang/controller/EduApplyBoardController.java
+++ donghang/src/main/java/com/tomato/donghang/controller/EduApplyBoardController.java
@@ -1,21 +1,30 @@
 package com.tomato.donghang.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.View;
 
 import com.cleopatra.protocol.data.DataRequest;
 import com.cleopatra.protocol.data.ParameterGroup;
+import com.cleopatra.spring.JSONDataView;
+import com.cleopatra.spring.UIView;
 import com.tomato.donghang.model.service.EduApplyBoardService;
 import com.tomato.donghang.model.vo.EduApplyBoardVO;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController 
 @RequiredArgsConstructor
+@Slf4j
 public class EduApplyBoardController {
 	private final EduApplyBoardService eduApplyBoardService;
 	@PostMapping("/ui/testajax.do")
@@ -24,8 +33,37 @@ public class EduApplyBoardController {
 		System.out.println("1");
 		return "hello ajax"; // RestController 즉 ajax 응답 
 	}
+	
+	@GetMapping("/ui/toBaordList.do")
+	public View BoardList(HttpServletRequest request, HttpServletResponse response, DataRequest dataRequest) {
+		return new UIView("/ui/eduApplyboardList.clx");
+	}
+	
+	
 	@PostMapping("/ui/createBoard.do")
 	public void createBoard(HttpServletRequest request, HttpServletResponse response,DataRequest dataRequest) { 
-		
 	}
+	@GetMapping("/ui/eduApplyboardList")
+	public View eduApplyboardList() {
+		log.info("a");
+		return new UIView("/ui/eduApplyboardList.clx");
+	}
+	
+	
+	
+	
+	@PostMapping("/ui/findBaordList.do")
+	public View findBoardList(HttpServletRequest request, HttpServletResponse response, DataRequest dataRequest) {
+		List<Map<String, Object>> data = eduApplyBoardService.findBaordList();
+		dataRequest.setResponse("ds1", data);
+		return new JSONDataView();
+	}
+	
+	@PostMapping("/ui/findBoardListByPage.do")
+	public View findBoardListByPage(HttpServletRequest request, HttpServletResponse response, DataRequest dataRequest) {
+	
+		return new JSONDataView();
+	}
+	
+	
 }
