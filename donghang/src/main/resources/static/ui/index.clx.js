460e2246ea4b8dc528670e3088b50244baacfd32
p-jisoo <vkrwltn@nate.com>
2023-08-27 14:53:34 +0900

    Feat : embedded app onBodyLoad event
    index에 진입시 home을 바로 띄우게 설정

--- donghang/src/main/resources/static/ui/index.clx.js
+++ donghang/src/main/resources/static/ui/index.clx.js
@@ -51,39 +51,10 @@
 			 */
 			function onNav1ItemClick(e) {
 				var nav1 = e.control;
-			//	var submission = app.lookup("sms1");
-			//	var navigationBar = app.lookup("nav1");
-			//	var count = navigationBar.getSelectedIndices().toString()
-			//	submission.setParameters("menu", count);
-			//	submission.send();
-			emded(e);
+				emded(e);
 			}
 
-			/*
-			 * 서브미션에서 submit-success 이벤트 발생 시 호출.
-			 * 통신이 성공하면 발생합니다.
-			 */
-			function onSms1SubmitSuccess2(e) {
-				var sms1 = e.control;
-				var number = sms1.getParameters("menu").toString();
-				//	if(number=="0"){
-				//		window.location.href="/";
-				switch (number) {
-					case "0":
-						window.location.href = "/";
-						break;
-					case "1":
-						window.location.href = "toBaordList.do";
-						break;
-					case "2":
-						window.location.href = "showmeapply.do";
-						break;
-					case "3":
-						window.location.href = "showmeapply.do";
-						break;
-				}
-				
-			}
+
 
 			/*
 			 * "회원가입  " 버튼에서 click 이벤트 발생 시 호출.
@@ -103,6 +74,16 @@
 			}
 
 			function onBodyLoad(e) {
+				var vcEmb = app.lookup("ea1");
+				cpr.core.App.load("home", function(/*cpr.core.App*/ loadedApp){
+					if(loadedApp){						
+						vcEmb.ready(function(/*cpr.controls.EmbeddedApp*/embApp){
+						})
+						vcEmb.app = loadedApp;
+						var app1 = vcEmb.app;
+						app1.getInstances();
+					}
+				});
 				var submission = app.lookup("sessioncheck");
 				submission.send();
 			}
@@ -265,243 +246,231 @@
 			container.setLayout(xYLayout_1);
 			
 			// UI Configuration
-			var group_1 = new cpr.controls.Container();
-			group_1.style.css({
-				"font-size" : "1rem"
+			var embeddedApp_1 = new cpr.controls.EmbeddedApp("ea1");
+			container.addChild(embeddedApp_1, {
+				"top": "237px",
+				"left": "21px",
+				"width": "1960px",
+				"height": "824px"
 			});
+			
+			var output_1 = new cpr.controls.Output();
+			output_1.value = "ITda";
+			output_1.style.css({
+				"color" : "#262626",
+				"font-weight" : "bolder",
+				"font-size" : "1.3rem"
+			});
+			container.addChild(output_1, {
+				"top": "16px",
+				"left": "162px",
+				"width": "100px",
+				"height": "44px"
+			});
+			
+			var output_2 = new cpr.controls.Output();
+			output_2.value = "Accompany";
+			output_2.style.css({
+				"color" : "black",
+				"font-weight" : "bold",
+				"font-size" : "1.3rem"
+			});
+			container.addChild(output_2, {
+				"top": "84px",
+				"left": "162px",
+				"width": "160px",
+				"height": "41px"
+			});
+			
+			var group_1 = new cpr.controls.Container();
 			var xYLayout_2 = new cpr.controls.layouts.XYLayout();
 			group_1.setLayout(xYLayout_2);
 			(function(container){
-				var embeddedApp_1 = new cpr.controls.EmbeddedApp("ea1");
-				container.addChild(embeddedApp_1, {
-					"top": "234px",
-					"left": "20px",
-					"width": "1960px",
-					"height": "824px"
-				});
-				var group_2 = new cpr.controls.Container();
-				var xYLayout_3 = new cpr.controls.layouts.XYLayout();
-				group_2.setLayout(xYLayout_3);
-				(function(container){
-					var button_1 = new cpr.controls.Button();
-					button_1.value = "회원정보 수정";
-					if(typeof onButtonClick4 == "function") {
-						button_1.addEventListener("click", onButtonClick4);
-					}
-					container.addChild(button_1, {
-						"top": "20px",
-						"left": "411px",
-						"width": "139px",
-						"height": "43px"
-					});
-					var button_2 = new cpr.controls.Button();
-					button_2.value = "마이페이지";
-					if(typeof onButtonClick5 == "function") {
-						button_2.addEventListener("click", onButtonClick5);
-					}
-					container.addChild(button_2, {
-						"top": "20px",
-						"left": "239px",
-						"width": "164px",
-						"height": "44px"
-					});
-					var button_3 = new cpr.controls.Button("temporary_btn");
-					button_3.value = "임시 회원탈퇴 버튼, 후에 마이페이지 내에 넣을 예정";
-					if(typeof onButtonClick3 == "function") {
-						button_3.addEventListener("click", onButtonClick3);
-					}
-					container.addChild(button_3, {
-						"top": "19px",
-						"left": "584px",
-						"width": "221px",
-						"height": "45px"
-					});
-					var button_4 = new cpr.controls.Button("btn_register");
-					button_4.value = "회원가입  ";
-					button_4.style.css({
-						"background-color" : "#FFFFFF",
-						"border-right-style" : "none",
-						"color" : "#4682A9",
-						"border-left-style" : "none",
-						"font-weight" : "bold",
-						"font-size" : "1rem",
-						"border-bottom-style" : "none",
-						"background-image" : "none",
-						"border-top-style" : "none"
-					});
-					if(typeof onButtonClick2 == "function") {
-						button_4.addEventListener("click", onButtonClick2);
-					}
-					container.addChild(button_4, {
-						"top": "20px",
-						"left": "1388px",
-						"width": "105px",
-						"height": "40px"
-					});
-					var button_5 = new cpr.controls.Button("mypage");
-					button_5.visible = false;
-					button_5.value = " 마이페이지 ";
-					button_5.style.css({
-						"background-color" : "#FFFFFF",
-						"border-right-style" : "none",
-						"color" : "#4682A9",
-						"border-left-style" : "none",
-						"font-weight" : "bold",
-						"font-size" : "1rem",
-						"border-bottom-style" : "none",
-						"background-image" : "none",
-						"border-top-style" : "none"
-					});
-					if(typeof onButtonClick == "function") {
-						button_5.addEventListener("click", onButtonClick);
-					}
-					container.addChild(button_5, {
-						"top": "20px",
-						"left": "1180px",
-						"width": "120px",
-						"height": "40px"
-					});
-					var button_6 = new cpr.controls.Button("login");
-					button_6.value = "  로그인   ";
-					button_6.style.css({
-						"background-color" : "#FFFFFF",
-						"border-right-style" : "none",
-						"color" : "#4682A9",
-						"border-left-style" : "none",
-						"font-weight" : "bold",
-						"font-size" : "1rem",
-						"border-bottom-style" : "none",
-						"background-image" : "none",
-						"border-top-style" : "none"
-					});
-					if(typeof onLoginClick == "function") {
-						button_6.addEventListener("click", onLoginClick);
-					}
-					if(typeof onLoginValueChange == "function") {
-						button_6.addEventListener("value-change", onLoginValueChange);
-					}
-					container.addChild(button_6, {
-						"top": "20px",
-						"left": "1299px",
-						"width": "90px",
-						"height": "40px"
-					});
-					var output_1 = new cpr.controls.Output("welcom");
-					output_1.visible = false;
-					output_1.value = "님 환영합니다.";
-					output_1.style.css({
-						"font-weight" : "bold",
-						"font-size" : "1rem"
-					});
-					if(typeof onWelcomValueChange == "function") {
-						output_1.addEventListener("value-change", onWelcomValueChange);
-					}
-					container.addChild(output_1, {
-						"top": "23px",
-						"left": "992px",
-						"width": "138px",
-						"height": "34px"
-					});
-					var output_2 = new cpr.controls.Output("whoName");
-					output_2.style.css({
-						"font-weight" : "bold",
-						"font-size" : "1rem",
-						"text-align" : "center"
-					});
-					output_2.bind("value").toDataMap(app.lookup("dm1"), "userName");
-					if(typeof onOutputValueChange == "function") {
-						output_2.addEventListener("value-change", onOutputValueChange);
-					}
-					container.addChild(output_2, {
-						"top": "17px",
-						"left": "852px",
-						"width": "130px",
-						"height": "41px"
-					});
-				})(group_2);
-				if(typeof onGroupClick2 == "function") {
-					group_2.addEventListener("click", onGroupClick2);
+				var button_1 = new cpr.controls.Button();
+				button_1.value = "회원정보 수정";
+				if(typeof onButtonClick4 == "function") {
+					button_1.addEventListener("click", onButtonClick4);
 				}
-				container.addChild(group_2, {
-					"top": "0px",
-					"right": "0px",
-					"left": "0px",
-					"height": "74px"
+				container.addChild(button_1, {
+					"top": "20px",
+					"left": "213px",
+					"width": "139px",
+					"height": "43px"
 				});
-				var navigationBar_1 = new cpr.controls.NavigationBar("nav1");
-				navigationBar_1.barItemSpacing = 150;
-				navigationBar_1.style.css({
-					"border-right-style" : "none",
-					"border-left-style" : "none",
-					"font-weight" : "bold",
-					"font-size" : "1.3rem",
-					"border-bottom-style" : "none",
-					"border-top-style" : "none"
-				});
-				(function(navigationBar_1){
-					navigationBar_1.setItemSet(app.lookup("ds1"), {
-						"label": "labal",
-						"value": "value"
-					});
-				})(navigationBar_1);
-				if(typeof onNav1ItemClick == "function") {
-					navigationBar_1.addEventListener("item-click", onNav1ItemClick);
+				var button_2 = new cpr.controls.Button();
+				button_2.value = "마이페이지";
+				if(typeof onButtonClick5 == "function") {
+					button_2.addEventListener("click", onButtonClick5);
 				}
-				if(typeof onNav1Click2 == "function") {
-					navigationBar_1.addEventListener("click", onNav1Click2);
-				}
-				if(typeof onNav1SelectionChange == "function") {
-					navigationBar_1.addEventListener("selection-change", onNav1SelectionChange);
-				}
-				container.addChild(navigationBar_1, {
-					"top": "73px",
-					"right": "369px",
-					"left": "580px",
-					"height": "154px"
-				});
-				var output_3 = new cpr.controls.Output();
-				output_3.value = "ITda";
-				output_3.style.css({
-					"color" : "#262626",
-					"font-weight" : "bolder",
-					"font-size" : "1.3rem"
-				});
-				container.addChild(output_3, {
-					"top": "84px",
-					"left": "274px",
-					"width": "207px",
+				container.addChild(button_2, {
+					"top": "20px",
+					"left": "19px",
+					"width": "164px",
 					"height": "44px"
 				});
-				var output_4 = new cpr.controls.Output();
-				output_4.value = "Accompany";
-				output_4.style.css({
-					"color" : "black",
-					"font-weight" : "bold",
-					"font-size" : "1.3rem"
+				var button_3 = new cpr.controls.Button("temporary_btn");
+				button_3.value = "임시 회원탈퇴 버튼, 후에 마이페이지 내에 넣을 예정";
+				if(typeof onButtonClick3 == "function") {
+					button_3.addEventListener("click", onButtonClick3);
+				}
+				container.addChild(button_3, {
+					"top": "19px",
+					"left": "584px",
+					"width": "221px",
+					"height": "45px"
 				});
+				var button_4 = new cpr.controls.Button("btn_register");
+				button_4.value = "회원가입  ";
+				button_4.style.css({
+					"background-color" : "#FFFFFF",
+					"border-right-style" : "none",
+					"color" : "#4682A9",
+					"border-left-style" : "none",
+					"font-weight" : "bold",
+					"font-size" : "1rem",
+					"border-bottom-style" : "none",
+					"background-image" : "none",
+					"border-top-style" : "none"
+				});
+				if(typeof onButtonClick2 == "function") {
+					button_4.addEventListener("click", onButtonClick2);
+				}
+				container.addChild(button_4, {
+					"top": "20px",
+					"left": "1388px",
+					"width": "105px",
+					"height": "40px"
+				});
+				var button_5 = new cpr.controls.Button("mypage");
+				button_5.visible = false;
+				button_5.value = " 마이페이지 ";
+				button_5.style.css({
+					"background-color" : "#FFFFFF",
+					"border-right-style" : "none",
+					"color" : "#4682A9",
+					"border-left-style" : "none",
+					"font-weight" : "bold",
+					"font-size" : "1rem",
+					"border-bottom-style" : "none",
+					"background-image" : "none",
+					"border-top-style" : "none"
+				});
+				if(typeof onButtonClick == "function") {
+					button_5.addEventListener("click", onButtonClick);
+				}
+				container.addChild(button_5, {
+					"top": "20px",
+					"left": "1180px",
+					"width": "120px",
+					"height": "40px"
+				});
+				var button_6 = new cpr.controls.Button("login");
+				button_6.value = "  로그인   ";
+				button_6.style.css({
+					"background-color" : "#FFFFFF",
+					"border-right-style" : "none",
+					"color" : "#4682A9",
+					"border-left-style" : "none",
+					"font-weight" : "bold",
+					"font-size" : "1rem",
+					"border-bottom-style" : "none",
+					"background-image" : "none",
+					"border-top-style" : "none"
+				});
+				if(typeof onLoginClick == "function") {
+					button_6.addEventListener("click", onLoginClick);
+				}
+				if(typeof onLoginValueChange == "function") {
+					button_6.addEventListener("value-change", onLoginValueChange);
+				}
+				container.addChild(button_6, {
+					"top": "20px",
+					"left": "1299px",
+					"width": "90px",
+					"height": "40px"
+				});
+				var output_3 = new cpr.controls.Output("welcom");
+				output_3.visible = false;
+				output_3.value = "님 환영합니다.";
+				output_3.style.css({
+					"font-weight" : "bold",
+					"font-size" : "1rem"
+				});
+				if(typeof onWelcomValueChange == "function") {
+					output_3.addEventListener("value-change", onWelcomValueChange);
+				}
+				container.addChild(output_3, {
+					"top": "23px",
+					"left": "992px",
+					"width": "138px",
+					"height": "34px"
+				});
+				var output_4 = new cpr.controls.Output("whoName");
+				output_4.style.css({
+					"font-weight" : "bold",
+					"font-size" : "1rem",
+					"text-align" : "center"
+				});
+				output_4.bind("value").toDataMap(app.lookup("dm1"), "userName");
+				if(typeof onOutputValueChange == "function") {
+					output_4.addEventListener("value-change", onOutputValueChange);
+				}
 				container.addChild(output_4, {
-					"top": "127px",
-					"left": "274px",
-					"width": "296px",
+					"top": "17px",
+					"left": "852px",
+					"width": "130px",
 					"height": "41px"
 				});
-				var image_1 = new cpr.controls.Image();
-				image_1.src = "theme/images/img/logo2_donghang.png";
-				container.addChild(image_1, {
-					"top": "73px",
-					"left": "129px",
-					"width": "135px",
-					"height": "102px"
-				});
 			})(group_1);
-			if(typeof onGroupClick == "function") {
-				group_1.addEventListener("click", onGroupClick);
+			if(typeof onGroupClick2 == "function") {
+				group_1.addEventListener("click", onGroupClick2);
 			}
 			container.addChild(group_1, {
-				"top": "0px",
-				"right": "0px",
-				"bottom": "0px",
-				"left": "0px"
+				"top": "1px",
+				"right": "-194px",
+				"left": "432px",
+				"height": "74px"
+			});
+			
+			var image_1 = new cpr.controls.Image();
+			image_1.src = "theme/images/img/logo2_donghang.png";
+			container.addChild(image_1, {
+				"top": "21px",
+				"left": "21px",
+				"width": "135px",
+				"height": "102px"
+			});
+			
+			var navigationBar_1 = new cpr.controls.NavigationBar("nav1");
+			navigationBar_1.barItemSpacing = 150;
+			navigationBar_1.style.css({
+				"border-right-style" : "none",
+				"border-left-style" : "none",
+				"font-weight" : "bold",
+				"font-size" : "1.3rem",
+				"border-bottom-style" : "none",
+				"border-top-style" : "none"
+			});
+			(function(navigationBar_1){
+				navigationBar_1.setItemSet(app.lookup("ds1"), {
+					"label": "labal",
+					"value": "value"
+				});
+			})(navigationBar_1);
+			if(typeof onNav1ItemClick == "function") {
+				navigationBar_1.addEventListener("item-click", onNav1ItemClick);
+			}
+			if(typeof onNav1Click2 == "function") {
+				navigationBar_1.addEventListener("click", onNav1Click2);
+			}
+			if(typeof onNav1SelectionChange == "function") {
+				navigationBar_1.addEventListener("selection-change", onNav1SelectionChange);
+			}
+			container.addChild(navigationBar_1, {
+				"top": "84px",
+				"right": "446px",
+				"left": "503px",
+				"height": "154px"
 			});
 			if(typeof onBodyLoad == "function"){
 				app.addEventListener("load", onBodyLoad);
